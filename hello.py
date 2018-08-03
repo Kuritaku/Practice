67f178de4c986f9c9640b4123bca89f1d9dccf02
Kuritaku <kurironaldo7@gmail.com>
2018-08-03 13:44:35 -0500

    add new file

---
+++ hello.py
@@ -0,0 +1,12 @@
+#  _*_ coding : utf-8  _*_
+
+class hello_world:
+
+    def hello(self,fname):
+      return print(' Hello World   '+str(fname))
+
+if __name__ == "__main__":
+   fname = 'XXX'
+   hello_world  = HW()
+   HW.hello(fname)
+
